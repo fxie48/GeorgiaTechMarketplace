0ef7e89deb2ad0e939ba5d44a592501c624f08c0
Pak, Neo <npak9@gatech.edu>
2023-09-09 23:11:07 -0400

    created randomized pokemon on home page

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,26 +1,55 @@
-import {StyleSheet, Text, View, Button} from 'react-native'
-import React, { useEffect } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import styles from './home.style'
-import { addDoc, collection } from 'firebase/firestore'
-import { FIREBASE_DB } from '../Firebase/firebase';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useCallback, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 
-const Home = () => {
+const pokePath = "https://pokeapi.co/api/v2/";
+const pokeQuery = "pokemon?limit=151&offset=0";
+const randomNumber = Math.floor(Math.random() * 151) + 1;
 
-    useEffect(() => {
-        addDoc(collection(FIREBASE_DB, 'todos'), {title: 'I am a test', done: false})
+export default function App() {
+  const [firstGenPokemonDetails, setfirstGenPokemonDetails] = useState([]);
 
-    }, []);
+  useEffect(() => {
+    const fetchFirstGenPokemons = async () => {
+      const firstGenPokemonIdsResponse = await fetch(`${pokePath}${pokeQuery}`);
+      const firstGenPokemonIdsBody = await firstGenPokemonIdsResponse.json();
 
+      const firstGenPokemonDetails = await Promise.all(
+        firstGenPokemonIdsBody.results.map(async (p) => {
+          const pDetails = await fetch(p.url);
+
+          return await pDetails.json();
+        })
+      );
+
+      setfirstGenPokemonDetails(firstGenPokemonDetails.slice(randomNumber, randomNumber + 1));
+    };
+
+    fetchFirstGenPokemons();
+  }, []);
+
+  const renderPokemon = ({ item }) => {
     return (
-        <SafeAreaView>
-            <View style = {styles.appBarWrapper}>
-                <View style={styles.appBar}>
-                    
-                </View>
-            </View>
-        </SafeAreaView>
-    )
-}
+      <View style={styles.pokemonContainer}>
+        <Text style={styles.pokemonTitle}>
+          {item.name.toUpperCase()}
+        </Text>
+        <Image
+          style={styles.pokemonSprite}
+          source={{
+            uri: item.sprites.front_default,
+          }}
+        />
+      </View>
+    );
+  };
 
-export default Home
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>First Gen Pokemons</Text>
+      <FlatList data={firstGenPokemonDetails} renderItem={renderPokemon} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
--- screens/home.style.js
+++ screens/home.style.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import {COLORS, SIZES} from "../constants/index";
 
 const styles = StyleSheet.create({
@@ -14,7 +14,28 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center"
-    }
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        marginTop: 60,
+    },
+    title: {
+        fontSize: 38,
+        alignSelf: "center",
+        marginBottom: 20,
+    },
+    pokemonContainer: { backgroundColor: "lightgrey", marginTop: 10 },
+    pokemonTitle: {
+        fontSize: 32,
+        alignSelf: "center",
+        marginTop: 10,
+    },
+    pokemonSprite: {
+        width: 200,
+        height: 200,
+        alignSelf: "center",
+    },
 })
 
 export default styles
